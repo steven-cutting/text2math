d959fa133e1b0124573338aed0883c5e150d78d1
Steven Cutting <steven.e.cutting@linux.com>
2016-03-11 19:17:39 -0600

    added adv_decode

--- text2math/__init__.py
+++ text2math/__init__.py
@@ -6,7 +6,7 @@ __copyright__ = "text2math Copyright (C) 2016  Steven Cutting"
 
 import cytoolz as tlz
 
-from text2math.raw2text import(remove_html_bits, decode_and_fix)
+from text2math.raw2text import(remove_html_bits, decode_and_fix, adv_decode)
 from text2math.text2tokens import(unigram, bigram, trigram, uni_and_bigram_tuples)
 from text2math.tokens2numbers import freq
 
