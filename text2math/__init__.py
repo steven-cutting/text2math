c9d230ad2c63fa22ed7ff2dec79b85d51e41fd98
Steven Cutting <steven.e.cutting@linux.com>
2016-03-17 05:48:11 -0500

    added ngram import

--- text2math/__init__.py
+++ text2math/__init__.py
@@ -7,10 +7,14 @@ __copyright__ = "text2math Copyright (C) 2016  Steven Cutting"
 import cytoolz as tlz
 
 from text2math.raw2text import(remove_html_bits, decode_and_fix, adv_decode)
-from text2math.text2tokens import(unigram, bigram, trigram, uni_and_bigram_tuples)
+from text2math.text2tokens import(ngram, unigram, bigram, trigram,
+                                  uni_and_bigram_tuples)
 from text2math.tokens2numbers import freq
 
 
-tknize_uni_n_bi = tlz.compose(tuple, uni_and_bigram_tuples, decode_and_fix, remove_html_bits)
+tknize_uni_n_bi = tlz.compose(tuple,
+                              uni_and_bigram_tuples,
+                              decode_and_fix,
+                              remove_html_bits)
 
 total_counts = tlz.compose(freq, tlz.concat)
